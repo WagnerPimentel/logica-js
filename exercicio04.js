0e6b4acb00928bb490250bc02aed2a77a3fe9126
Wagner Pimentel <142364404+WagnerPimentel@users.noreply.github.com>
2023-11-14 22:42:28 +0000

    toperatualização

--- exercicio04.js
+++ exercicio04.js
@@ -1,5 +1,14 @@
 // Altere o programa anterior para mostrar no final a soma dos números.
 
-for(let i = 1; i <= 10; i++){
-    console.log(i)
+let inicio, fim, soma
+
+inicio = 8
+fim = 16
+
+while (inicio <= fim) {
+    console.log(inicio)
+    soma = inicio + inicio
+    inicio++
 }
+
+console.log("Soma dos números: " + soma)
